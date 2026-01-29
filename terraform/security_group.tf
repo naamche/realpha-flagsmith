1803af9c05bfdcc1d44c152556003428953bf984
Ravi <rabigiri18@gmail.com>
2026-01-29 17:25:07 +0545

    chore: remove unnecessary rule from launch template sg

--- terraform/security_group.tf
+++ terraform/security_group.tf
@@ -3,13 +3,6 @@ resource "aws_security_group" "ec2_security_group" {
   vpc_id = data.aws_vpc.vpc.id
 
   ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = [data.aws_vpc.vpc.cidr_block]
-  }
-
-  ingress {
     from_port   = var.application_port
     to_port     = var.application_port
     protocol    = "tcp"
@@ -19,13 +12,6 @@ resource "aws_security_group" "ec2_security_group" {
   egress {
     from_port   = 0
     to_port     = 65535
-    protocol    = "tcp"
-    cidr_blocks = [data.aws_vpc.vpc.cidr_block]
-  }
-
-  egress {
-    from_port   = 443
-    to_port     = 443
     protocol    = "tcp"
     cidr_blocks = [var.all_addresses]
   }
